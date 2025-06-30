2f81002a4cbb2700af0d99106137e0f4317bc26d
efrain <efrainperalta.f@gmail.com>
2025-06-29 21:03:21 -0400

    feat(frontend): implementar interfaz para el contenido de una materia

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { Home } from './features/home/home';
 import { SubjectsDashboard } from './features/subjects-dashboard/subjects-dashboard';
+import { SubjectContent } from './features/subject-content/subject-content';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'materias', component: SubjectsDashboard },
+  { path: 'materias/:id/contenido', component: SubjectContent },
 ];
